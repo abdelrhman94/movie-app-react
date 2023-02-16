e437844aa09e74755af2a1b7cc0c737b732145ff
Abdelrhman Farag <abdelrhman.s.farag@gmail.com>
2023-02-16 17:40:31 +0200

    setup component structer and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
 
-const App = () => <div>App</div>;
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './components';
+
+const App = () => (
+  <div>
+    <CssBaseline />
+    <NavBar />
+    <main>
+      <Routes>
+        <Route exact path="/movie/:id" element={<MovieInformation />} />
+        <Route path="/actors/:id" element={<Actors />} />
+        <Route exact path="/" element={<Movies />} />
+        <Route path="/profile/:id" element={<Profile />} />
+      </Routes>
+    </main>
+  </div>
+);
 
 export default App;
