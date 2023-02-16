2b6dc9022ceb604ea6667348317a472e93f1fb67
Abdelrhman Farag <abdelrhman.s.farag@gmail.com>
2023-02-16 19:32:41 +0200

    complete NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { useTheme } from '@mui/styles';
+
+import useStyle from './styles';
 
 const NavBar = () => {
   console.log('NavBar');
+  const classes = useStyle();
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const theme = useTheme();
+  const isAuthenticated = true;
   return (
-    <div>NavBar</div>
+    <AppBar position="fixed">
+      <Toolbar className={classes.toolbar}>
+        {isMobile && (
+          <IconButton color="inherit" edge="start" style={{ outline: 'none' }} onClick={() => {}} className={classes.menuButton}>
+            <Menu />
+          </IconButton>
+        )}
+        <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+          {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 /> }
+
+        </IconButton>
+        {!isMobile && 'Search...'}
+        <div>
+          {!isAuthenticated ? (
+            <Button color="inherit" onClick={() => {}}>Login &nbsp; <AccountCircle />  </Button>
+          ) : (
+            <Button
+              color="inherit"
+              component={Link}
+              to="/profile/:id"
+              className={classes.linkButton}
+              onClick={() => {}}
+            >
+              {!isMobile && <>My Movies &nbsp;  </>}
+              <Avatar
+                style={{ width: 30, height: 30 }}
+                alt="profile"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpWvXdcjNuTkrkDCYKZRtWwZ-emiiDJdP6sUb7VRshRA&s"
+              />
+            </Button>
+          )}
+        </div>
+        {isMobile && 'Search...'}
+      </Toolbar>
+    </AppBar>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
+const theme = createTheme({});
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  </ThemeProvider>,
 );
